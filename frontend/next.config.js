3fc132936e4d902b59482c6b09551691911e64b0
Rohald van Merode <rohald_89@live.nl>
2022-04-15 14:19:21 +0200

    added localization to both front and backend

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ['default', 'en', 'nl'],
+    defaultLocale: 'default',
+  },
+  trailingSlash: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ frontend/pages/_middleware.js
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+const PUBLIC_FILE = /\.(.*)$/;
+
+export function middleware(request) {
+  const shouldHandleLocale =
+    !PUBLIC_FILE.test(request.nextUrl.pathname) &&
+    !request.nextUrl.pathname.includes('/api/') &&
+    request.nextUrl.locale === 'default';
+
+  if (shouldHandleLocale) {
+    const url = request.nextUrl.clone();
+    url.pathname = `/en${request.nextUrl.pathname}`;
+    return NextResponse.redirect(url);
+  }
+
+  return undefined;
+}
